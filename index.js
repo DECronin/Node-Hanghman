97effcac0831f3e2e73ac3967e965e0ce02eb49f
Dana Cronin <cronin43224235@gmail.com>
2019-11-29 13:57:25 -0500

    psuedocoding

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // The file containing the logic for the course of the game, which depends on Word.js and:
-
 // Randomly selects a word and uses the Word constructor to store it
-
 // Prompts the user for each guess and keeps track of the user's remaining guesses
+
+
+//count number of guesses
+//retrieve random word from array
+//display word in blanks\
+//retrieve guess
+//compare guess
+//display and decrement gueses count(?)
+//update display and count with each guess
+//new word/round when no more blanks or guesscount hits 0
+//new game when all words in array have commenced
+/////////// offer new game retrieval?
+/////////// hint extention?
+/////////// exit game option?
