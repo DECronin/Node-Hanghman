28f93d3ce73e14725d39b41969518dee869218fa
Dana Cronin <cronin43224235@gmail.com>
2019-12-02 16:47:52 -0500

    case-logic

--- letter.js
+++ letter.js
@@ -16,10 +16,19 @@
 
 class Letter {
     constructor (letter) {
-        this.letter = letter;
+        this.char = letter;
+        this.pastGuess = false;
+    }
+    display(){
+        switch (this){
+            case this.char === " ": return " "; break;
+            case this.pastGuess: return this.char; break;
+            default: return "_"        }
+    }
+    comparison(guess){
+        if (guess === this.char || this.char === " ") {this.pastGuess = true}
     }
 }
-
 
 module.exports = Letter
 // recieve guess, incorrects and word
